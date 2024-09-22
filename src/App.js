7332b6eb60154e9ca50cf1c2a492999aef1c38b6
Othmane Assari <othmane.assari@gmail.com>
2024-09-22 02:36:46 +0200

    Design and buttons completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import logotr from "../src/assets/icon/logotr.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex flex-col items-center justify-between h-[50vh]">
+        <div className="flex justify-center w-full h-screen">
+          <img className="h-[180px] w-[180px] mt-5" src={logotr} alt=""></img>
+        </div>
+        <div className="flex justify-center gap-4 mt-12 mb-12">
+          <input
+            type="text"
+            placeholder="Temperature (°C)"
+            className="border-2 border-[#31C48D] rounded-lg p-2 w-48"
+          />
+          <input
+            type="text"
+            placeholder="Precipitation (mm)"
+            className="border-2 border-[#31C48D] rounded-lg p-2 w-48"
+          />
+        </div>
+        <button className="bg-black text-white rounded-lg p-2 w-48 transition duration-300 ease-in-out hover:bg-white hover:text-black">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
